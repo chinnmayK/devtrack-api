b4b136b872661bd4edb81faa28706719021b83fe
chinnmayk <chinnmayk@gmail.com>
2026-01-06 17:41:01 +0530

    Update homepage text v2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>🚀 DevTrack API . . .</h1><p>Status: Running ✅</p>');
+  res.send('<h1>🚀 DevTrack API . . .</h1><p>Status: Running Successfully 100% ✅</p>');
 });
 
 app.get('/health', (req, res) => {
